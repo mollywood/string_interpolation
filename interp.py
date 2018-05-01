670afd6ba0b865358c09c733df699b907dca6c3e
Molly Wood <mollywood@Interzone.localdomain>
2018-05-01 12:57:45 -0500

    inital commit

---
+++ interp.py
@@ -0,0 +1,6 @@
+# ask for user input
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+
+# print message using Input
+print(f"Hello, my name is {firstName} {lastName}.")
